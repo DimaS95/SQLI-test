60d51d434358552a22c93ad406297e2eb29ec4ea
Dmitry Smuschenko <dsmusenko@gmail.com>
2018-03-12 13:00:38 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,55 @@
+<?php
+//Note. that we use mysqli only to show how to work with SQLI. In your real projects we strongly recommend you to use PDO. 
+// enter your DB-data
+$localhost = '';
+$user = '';
+$password = '';
+$db = '';
+
+$mysqli = mysqli_connect("localhost", "root", "", "simpleapp");
+
+if($_GET['id']) {
+	$id = (int)$_GET['id'];
+	$news = mysqli_query($mysqli, "SELECT * FROM `news` WHERE `id`=$id");
+} 
+else {
+	$news = mysqli_query($mysqli, "SELECT * FROM `news`");
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>News</title>
+	
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+<div class="container">
+  <h2>News</h2>
+         <table class="table">
+    <thead>
+      <tr>
+      	<th>ID</th>
+        <th>Text</th>
+       
+      </tr>
+    </thead>
+    <tbody>
+    <?php 
+ 	 while ($n = mysqli_fetch_assoc($news)) { ?>
+    	<tr>
+        <td><?= $n["id"]; ?></td>
+        <td><?= $n["text"]; ?></td>
+    </tr>
+    <?php 
+    } ?>
+    </tbody>
+  </table>
+ 
+</div>
+</body>
+</html>
---
+++ simpleapp.sql
@@ -0,0 +1,102 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.3
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1:3306
+-- Время создания: Мар 12 2018 г., 13:54
+-- Версия сервера: 5.6.37
+-- Версия PHP: 5.6.31
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `simpleapp`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `news`
+--
+
+CREATE TABLE `news` (
+  `id` int(11) NOT NULL,
+  `text` varchar(255) DEFAULT NULL,
+  `public` tinyint(4) DEFAULT NULL,
+  `price` int(11) DEFAULT NULL,
+  `popularity` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `news`
+--
+
+INSERT INTO `news` (`id`, `text`, `public`, `price`, `popularity`) VALUES
+(1, 'text1', 1, 123, '321'),
+(2, 'text2', 1, 321, NULL),
+(3, 'text3', 0, NULL, NULL),
+(4, 'text4', 1, 12343, NULL),
+(5, 'text5', 1, 1232131, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`) VALUES
+(1, '123', '321');
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `news`
+--
+ALTER TABLE `news`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `news`
+--
+ALTER TABLE `news`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+--
+-- AUTO_INCREMENT для таблицы `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
